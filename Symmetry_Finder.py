77349f19fa3a267da81ccff74324620680319d73
Ellie Copps <ecopps@middlebury.edu>
2020-06-25 09:47:12 -0400

    Added functionality to test multiple seeds

--- Symmetry_Finder.py
+++ Symmetry_Finder.py
@@ -10,42 +10,49 @@ import tfim
 parser = argparse.ArgumentParser()
 parser.add_argument('N', type=int, help='Number of spins')
 parser.add_argument('seed', type=int, help='Jij matrix seed')
+parser.add_argument('seed_range', type=int, help='Number of seeds to test')
 parser.add_argument('max_energy', type=int, help='Maximum energy level to search for symmetries in')
 args = parser.parse_args()
     
 N = args.N
 seed = args.seed
+seed_range = args.seed_range
 max_energy = args.max_energy
 
+for seed in range(seed, (seed+seed_range)):
 
-PBC = True
-L = [N]
-lattice = tfim.Lattice(L, PBC)
-J = 1
+    print 'Seed: ', seed
 
-Jij = tfim.Jij_instance(N,J,dist="bimodal",seed=seed,even=True)
-basis = tfim.IsingBasis(lattice)
+    PBC = True
+    L = [N]
+    lattice = tfim.Lattice(L, PBC)
+    J = 1
 
-
-grouped_configurations = sym_mod.grouped_configs(basis, Jij)
-
-
-transformations = sym_mod.list_transformations(grouped_configurations, max_energy, N, basis)
+    Jij = tfim.Jij_instance(N,J,dist="bimodal",seed=seed,even=True)
+    basis = tfim.IsingBasis(lattice)
 
 
-symmetries = sym_mod.symmetry(transformations, grouped_configurations, basis)
+    grouped_configurations = sym_mod.grouped_configs(basis, Jij)
 
 
-sorted_list = sym_mod.list_sorter(symmetries, N)
+    transformations = sym_mod.list_transformations(grouped_configurations, max_energy, N, basis)
+
+
+    symmetries = sym_mod.symmetry(transformations, grouped_configurations, basis)
+
+
+    sorted_list = sym_mod.list_sorter(symmetries, N)
 
 
 
-simplified_sym = sym_mod.remove_combo(sorted_list)
+    simplified_sym = sym_mod.remove_combo(sorted_list)
 
 
 
-sorted_sym = symmetry_types.main(seed, N)
+    sorted_sym = symmetry_types.main(seed, N)
 
 
-classified_sym = sym_mod.symmetry_sorter(simplified_sym, sorted_sym)
+    classified_sym = sym_mod.symmetry_sorter(simplified_sym, sorted_sym)
+    
+    print ' '
 
