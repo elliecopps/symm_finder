c1c34bc39c57f4dd90043014771f555eb9008e4f
Ellie Copps <ecopps@middlebury.edu>
2020-06-25 12:10:34 -0400

    Moved basis calculation outside of for loop

--- Symmetry_Finder.py
+++ Symmetry_Finder.py
@@ -19,38 +19,29 @@ seed = args.seed
 seed_range = args.seed_range
 max_energy = args.max_energy
 
+PBC = True
+L = [N]
+lattice = tfim.Lattice(L, PBC)
+J = 1
+basis = tfim.IsingBasis(lattice)
+
 for seed in range(seed, (seed+seed_range)):
 
     print 'Seed: ', seed
 
-    PBC = True
-    L = [N]
-    lattice = tfim.Lattice(L, PBC)
-    J = 1
-
     Jij = tfim.Jij_instance(N,J,dist="bimodal",seed=seed,even=True)
-    basis = tfim.IsingBasis(lattice)
-
 
     grouped_configurations = sym_mod.grouped_configs(basis, Jij)
 
-
     transformations = sym_mod.list_transformations(grouped_configurations, max_energy, N, basis)
-
 
     symmetries = sym_mod.symmetry(transformations, grouped_configurations, basis)
 
-
     sorted_list = sym_mod.list_sorter(symmetries, N)
-
-
 
     simplified_sym = sym_mod.remove_combo(sorted_list)
 
-
-
     sorted_sym = symmetry_types.main(seed, N)
-
 
     classified_sym = sym_mod.symmetry_sorter(simplified_sym, sorted_sym)
     
