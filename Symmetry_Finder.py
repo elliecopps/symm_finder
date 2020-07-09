e0a4e8e5db7777b4f8ab46407c82b7782fab12c5
Ellie Copps <ecopps@middlebury.edu>
2020-07-09 08:38:21 -0400

    Updated write to file

--- Symmetry_Finder.py
+++ Symmetry_Finder.py
@@ -34,9 +34,6 @@ def main():
 
 
         print 'Seed: ', seed
-        
-        f.write('Seed: ' + str(seed) + '\n')
-        g.write('Seed: ' + str(seed) + '\n')
 
         Jij = tfim.Jij_instance(N,J,dist="bimodal",seed=seed,even=True)
         
@@ -44,19 +41,11 @@ def main():
 
         grouped_configurations = sym_mod.grouped_configs(Energies)
         
-        g.write("Ground state degeneracy: " + str(grouped_configurations[0]) + '\n')
-        
         transformations = sym_mod.list_transformations(grouped_configurations, max_energy, N, basis)
 
         symmetries = sym_mod.symmetry(transformations[0], grouped_configurations, basis)
         
         if len(symmetries) == 0:
-            f.write('No symmetries' + '\n')
-            f.write('\n')
-            g.write('Degeneracy pairs related by symmetries: ' + '\n')
-            g.write('     ' + 'None' + '\n')
-            g.write('\n')
-            
             continue
 
         sorted_list = sym_mod.list_sorter(symmetries, N)
@@ -66,26 +55,16 @@ def main():
         sorted_sym = symmetry_types.main(seed, N)
         
         classified_sym = sym_mod.symmetry_sorter(simplified_sym, sorted_sym)
+        f.write(str(seed)+'\n')
+        f.write(str(classified_sym)+'\n')
         
         if max_energy == 1:
-            degen_clusters = sym_mod.degen_sym(transformations[1], classified_sym)
+            degen_matrix = sym_mod.degen_sym(transformations[1], grouped_configurations[0], classified_sym)
             
-            g.write('Degeneracy pairs related by symmetries: ')
-            if len(degen_clusters) == 0:
-                g.write('None' + '\n')
-            else:
-                for cluster in degen_clusters:
-                    g.write('\n' + '     ' + str(cluster[0]) + ' , ' + str(cluster[1]) + '\n')
-                    g.write('     ' + 'Transformation: ' + str(cluster[2]) + '\n')
-                    g.write('\n')
-        
-        f.write('Swap symmetries: ' + str(classified_sym[0]) + '\n')
-        f.write('Anti-swap symmetries: ' + str(classified_sym[1]) + '\n')
-        f.write('Other: ' + str(classified_sym[2]) + '\n')
-        f.write('\n')
-        
-        print ' '
-        
+            clusters = sym_mod.cluster(degen_matrix, grouped_configurations[0])
+            if len(clusters) != 0:
+                g.write(str(seed) + '\n')
+                g.write(str(clusters) + '\n')
     f.close()
     g.close()
 
